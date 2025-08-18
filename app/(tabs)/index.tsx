50443437a0b36ab7a46dc730129bbf270f341d53
ranselorm <ranselorm@outlook.com>
2025-08-18 21:27:47 +0000

    Add todos query to Home component and log results

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,8 +1,12 @@
+import { api } from "@/convex/_generated/api";
 import useTheme from "@/hooks/useTheme";
+import { useQuery } from "convex/react";
 import React from "react";
 import { Text, TouchableOpacity, View } from "react-native";
 
 const Home = () => {
+  const todos = useQuery(api.todos.getTodos);
+  console.log(todos);
   const { toggleDarkMode } = useTheme();
   return (
     <View>
